8b55318f0daab985b76511e3fb93d81b5f2d9d81
Cali Tabilas <ctabilas23@gmail.com>
2019-07-02 16:29:13 -0400

    currency converter controller function completed

--- helperFunctions/helpers.js
+++ helperFunctions/helpers.js
@@ -1,21 +1,15 @@
 const axios = require('axios');
-const request = require('request');
+// const request = require('request');
 
 const getBitcoinData = cb => {
-  // request(`http://api.coindesk.com/v1/bpi/`, (err, res, body) => {
-  //   if (err) {
-  //     console.log('helperFunction - err', err);
-  //     cb(err);
-  //   } else {
-  //     console.log('helperFunction - sucess body', body);
-  //     cb(null, body);
-  //   }
-  // });
   axios
-    .get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-04-02&end=2019-07-02`)
+    .get(
+      `https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-04-02&end=2019-07-02`
+    )
     .then(data => {
       console.log('data from Axios request from Helpers', data.data);
-      cb(null, data);
+
+      cb(null, data.data.bpi);
     })
     .catch(err => {
       console.log('err in axios request from Helpers', err);
--- server/controller.js
+++ server/controller.js
@@ -2,17 +2,26 @@ const { getBitcoinData } = require('../helperFunctions/helpers.js');
 const axios = require('axios');
 
 module.exports.retrieve = (req, res) => {
+  getBitcoinData((err, results) => {
+    if (err) {
+      err => res.send().status(500);
+    } else {
+      res.send(results).status(200);
+    }
+  });
+};
+
+module.exports.convert = (req, res) => {
+  const { currency } = req.body;
+
   axios
     .get(
-      `https://api.coindesk.com/v1/bpi/historical/close.json?start=2019-04-02&end=2019-07-02`
+      `https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}&start=2019-04-02&end=2019-07-02`
     )
     .then(data => {
-      console.log('data from Axios request from Helpers', data.data.bpi);
-      const results = data.data.bpi;
-      res.send(results).status(200);
+      res.send(data).status(200);
     })
     .catch(err => {
-      console.log('err in axios request from Helpers', err);
-      res.send(err).status(500);
+      console.log('err in convert', err);
     });
 };
