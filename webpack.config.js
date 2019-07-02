6f81e62e49f7c544ee1b6367066c3641fff1e2a2
Cali Tabilas <ctabilas23@gmail.com>
2019-07-02 12:12:20 -0400

    modules are there

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './client/src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './client/dist')
+  }
+};
